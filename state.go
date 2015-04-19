b5d60d015d8716357dfce704e52fcfaa62f4f6b9
Karl Matthias <kmatthias@newrelic.com>
2015-04-19 16:39:20 -0700

    Try to improve convergence times by shipping more packets/try.

--- state.go
+++ state.go
@@ -474,7 +474,7 @@ func (m *Memberlist) gossip() {
 
 	// We try back-to-back to get a few broadcasts
 
-	for i := 0; i < len(kNodes); i++ {
+	for i := 0; i < 10; i++ {
 		// Get any pending broadcasts
 		msgs := m.getBroadcasts(compoundOverhead, bytesAvail)
 		if len(msgs) == 0 {
